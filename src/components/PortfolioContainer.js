ebb39d0d5c3c0b23a4f2d480932dd6e47af63bb1
Joseph Baca <josephcbaca@gmail.com>
2020-08-18 21:28:13 -0700

    Removed Blog page and switched out About for Projects.

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -1,39 +1,37 @@
 import React, { useState } from "react";
 import NavTabs from "./NavTabs";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import Blog from "./pages/Blog";
+import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 
 
 const Portfolio = () => {
-const [currentPage, setCurrentPage] = useState("Home")
+  const [currentPage, setCurrentPage] = useState("Home")
 
   let handlePageChange = setCurrentPage
 
   let renderPage = () => {
-switch (currentPage) {
-  case "Home":
-    return <Home />;
-  case "About":
-    return <About />;
-  case "Contact":
-    return <Contact />;
-  default:
-    console.log("err")
-}
+    switch (currentPage) {
+      case "Home":
+        return <Home />;
+      case "About":
+        return <Projects />;
+      case "Contact":
+        return <Contact />;
+      default:
+        console.log("err")
+    }
   };
 
-
-    return (
-      <div>
-        <NavTabs
-          currentPage={currentPage}
-          handlePageChange={handlePageChange}
-        />
-        {renderPage()}
-      </div>
-    );
+  return (
+    <div>
+      <NavTabs
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+      {renderPage()}
+    </div>
+  );
 }
 
 
