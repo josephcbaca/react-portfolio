849a7e233d03aac4b6b052a27efe6da65b0eda98
Joseph Baca <josephcbaca@gmail.com>
2020-08-20 22:23:05 -0700

    Completed db object for ImageCard information.

---
+++ src/components/db.js
@@ -0,0 +1,48 @@
+let projects = [
+    {
+        id: 1,
+        img: "../assets/projects/brewr.jpg",
+        alt: "Screenshot of brewR project.",
+        h5: "brewR",
+        siteHref: "https://brewrs.herokuapp.com/",
+        ghHref: "https://github.com/JMantis0/brewR"
+    }, {
+        id: 2,
+        img: "../assets/projects/DecisionsDecisions.jpg",
+        alt: "Screenshot of Decisions Decisions project.",
+        h5: "Decisions Decisions",
+        siteHref: "https://josephcbaca.github.io/DecisionsDecisions/",
+        ghHref: "https://github.com/josephcbaca/DecisionsDecisions"
+    }, {
+        id: 3,
+        img: "../assets/projects/burgermeister.jpg",
+        alt: "Screenshot of The Burgermeister project.",
+        h5: "The Burgermeister",
+        siteHref: "https://immense-thicket-40150.herokuapp.com/",
+        ghHref: "https://github.com/josephcbaca/Burgermeisterz-Revenge"
+    }, {
+        id: 4,
+        img: "../assets/projects/DayPlanner.jpg",
+        alt: "Screenshot of Personal Day Planner project.",
+        h5: "Personal Day Planner",
+        siteHref: "https://josephcbaca.github.io/PersonalDayPlanner/",
+        ghHref: "https://github.com/josephcbaca/PersonalDayPlanner"
+    }, {
+        id: 5,
+        img: "../assets/projects/WeatherDashboard.jpg",
+        alt: "Screenshot of Weather Dashboard project.",
+        h5: "Weather Dashboard",
+        siteHref: "https://josephcbaca.github.io/WeatherDashboard/",
+        ghHref: "https://github.com/josephcbaca/WeatherDashboard"
+    }, {
+        id: 6,
+        img: "../assets/projects/WeatherDashboard.jpg",
+        alt: "Screenshot of Password Generator project.",
+        h5: "Password Generator",
+        siteHref: "https://josephcbaca.github.io/WeatherDashboard/",
+        ghHref: "https://github.com/josephcbaca/WeatherDashboard"
+    }
+];
+
+
+    export default projects
