3081ae23a649abe8e9d205c9eeb67896b964e50b
Joseph Baca <josephcbaca@gmail.com>
2020-08-22 05:37:26 -0700

    Updates

---
+++ precache-manifest.92ee429473781f53d73ec4b533372eb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d0e3860d98e266f3c139bcf5b874f9",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "7bd28088e7fb4c403ad9",
+    "url": "/react-portfolio/static/css/main.f1d12701.chunk.css"
+  },
+  {
+    "revision": "81c3cc764dffd0b57a7e",
+    "url": "/react-portfolio/static/js/2.4402dc2b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.4402dc2b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bd28088e7fb4c403ad9",
+    "url": "/react-portfolio/static/js/main.a02634e0.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "3361136c9a8e5cc0b103902e0ebbf1b0",
+    "url": "/react-portfolio/static/media/barney_img.3361136c.jpg"
+  }
+]);
